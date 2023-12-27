d2b0b867a29fbf1956c98361a1557dcefb68b5c2
Aswathy <aswathyanuramar@gmail.com>
2023-12-27 11:36:46 +0530

    Zero logic added

--- src/components/CounterbyX.jsx
+++ src/components/CounterbyX.jsx
@@ -3,19 +3,24 @@ import { useState } from "react";
 function CounterByX(props) {
   const { x } = props;
   const [counter, setCounter] = useState(0);
-  function add() {
+  const add = () => {
     setCounter((old) => old + x);
-  }
-  function sub() {
-    setCounter((old) => old - x);
-  }
+  };
 
   return (
     <>
       <br />
       <button onClick={add}>+</button>
       <div>{counter}</div>
-      <button onClick={sub}>-</button>
+      <button
+        onClick={() => {
+          if (counter != 0) {
+            setCounter((old) => old - x);
+          }
+        }}
+      >
+        -
+      </button>
     </>
   );
 }
